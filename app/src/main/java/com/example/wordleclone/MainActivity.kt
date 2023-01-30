a444274e56272e88616a752a97e152e02a2d1335
nilmo-contreras <noc2@njit.edu>
2023-01-30 13:07:07 -0500

    Added FourLetterWordList.kt and checkGuess function

--- app/src/main/java/com/example/wordleclone/MainActivity.kt
+++ app/src/main/java/com/example/wordleclone/MainActivity.kt
@@ -4,8 +4,39 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    var attemptsCounter = 0
+    private val wordToGuess = FourLetterWordList.getRandomFourLetterWord()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+
+    /**
+     * Parameters / Fields:
+     *   wordToGuess : String - the target word the user is trying to guess
+     *   guess : String - what the user entered as their guess
+     *
+     * Returns a String of 'O', '+', and 'X', where:
+     *   'O' represents the right letter in the right place
+     *   '+' represents the right letter in the wrong place
+     *   'X' represents a letter not in the target word
+     */
+    private fun checkGuess(guess: String) : String {
+        var result = ""
+        for (i in 0..3) {
+            if (guess[i] == wordToGuess[i]) {
+                result += "O"
+            }
+            else if (guess[i] in wordToGuess) {
+                result += "+"
+            }
+            else {
+                result += "X"
+            }
+        }
+        return result
     }
 }
